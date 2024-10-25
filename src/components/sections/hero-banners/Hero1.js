01c3dab1d3b9cd8f9f319bce992c1dbfd6511c59
Sam-WebP <113742608+Sam-WebP@users.noreply.github.com>
2024-10-25 17:13:03 +1100

    feat: add Hero1 banner section with basic structure

---
+++ src/components/sections/hero-banners/Hero1.js
@@ -0,0 +1,16 @@
+"use client";
+
+import React, { useEffect } from "react";
+
+const Hero1 = () => {
+  return (
+    <section data-aos="fade-up">
+      {/* banner section  */}
+      <div className="container mx-auto px-4">
+        <p>Hello World</p>
+      </div>
+    </section>
+  );
+};
+
+export default Hero1;
