3408333a4830fb455e2086acf34a96cf10ebaddc
Sam-WebP <113742608+Sam-WebP@users.noreply.github.com>
2024-10-25 17:08:11 +1100

    feat: implement main homepage with PageWrapper

---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+import Homepage from "@/components/layout/main/Homepage";
+import PageWrapper from "@/components/shared/wrappers/PageWrapper";
+
+export default function Home() {
+  return (
+    <PageWrapper>
+      <main>
+        <Homepage />
+      </main>
+    </PageWrapper>
+  );
+}
