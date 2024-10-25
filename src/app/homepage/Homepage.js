7c4f007e0a8bec12b86863c37a0ef2800677b25a
Sam-WebP <113742608+Sam-WebP@users.noreply.github.com>
2024-10-25 17:16:04 +1100

    feat: add basic homepage structure with hero component

---
+++ src/app/homepage/Homepage.js
@@ -0,0 +1,12 @@
+import Hero1 from "@/components/sections/hero-banners/Hero1";
+
+import React from "react";
+const Homepage = () => {
+  return (
+    <>
+      <Hero1 />
+    </>
+  );
+};
+
+export default Homepage;
