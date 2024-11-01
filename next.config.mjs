2237e1281ce198bfe80cafe7ab82644d18e4cfcf
Sam-WebP <113742608+Sam-WebP@users.noreply.github.com>
2024-11-01 22:08:07 +1100

    chore: configure video file support and TypeScript declarations

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(mp4|webm)$/i,
+      type: 'asset/resource',
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
