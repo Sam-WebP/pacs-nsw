d4ec69e2134bb4496b5a400f3901f7ac30b292e0
Sam-WebP <113742608+Sam-WebP@users.noreply.github.com>
2024-11-01 22:05:11 +1100

    feat: add reusable heading component with colour variants

---
+++ src/components/shared/headings/HeadingLg.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+interface HeadingLgProps {
+  children: React.ReactNode;
+  color?: 'white' | 'default';
+}
+
+const HeadingLg: React.FC<HeadingLgProps> = ({ children, color }) => {
+  return (
+    <div>
+      <h1
+        className={`text-3xl ${
+          color === "white"
+            ? "text-whiteColor"
+            : "text-blackColor dark:text-blackColor-dark"
+        } md:text-5xl lg:text-size-50 2xl:text-5xl leading-10 md:leading-18 lg:leading-62px 2xl:leading-18 md:tracking-half lg:tracking-normal 2xl:tracking-half font-bold mb-15px`}
+      >
+        {children}
+      </h1>
+    </div>
+  );
+};
+
+export default HeadingLg;
