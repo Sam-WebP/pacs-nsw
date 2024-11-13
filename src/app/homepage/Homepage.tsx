260a5eb56271be974558fc33c6f46149f1275810
Sam-WebP <113742608+Sam-WebP@users.noreply.github.com>
2024-11-13 17:21:04 +1100

    chore: add Program component to Homepage

--- src/app/homepage/Homepage.tsx
+++ src/app/homepage/Homepage.tsx
@@ -1,5 +1,6 @@
 import Hero1 from "@/components/sections/hero-banners/Hero1";
 import About from "@/components/sections/about/About";
+import Programs from "@/components/sections/programs/Programs";
 import React from "react";
 
 const Homepage: React.FC = () => {
@@ -7,6 +8,7 @@ const Homepage: React.FC = () => {
     <>
       <Hero1 />
       <About />
+      <Programs />
     </>
   );
 };
