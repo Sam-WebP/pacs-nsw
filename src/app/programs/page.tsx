aa800bfd2c88757530f5641d4b9accb19cd49880
Sam-WebP <113742608+Sam-WebP@users.noreply.github.com>
2024-11-18 12:35:32 +1100

    feat: add standalone programs page

---
+++ src/app/programs/page.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import PageWrapper from "@/components/shared/wrappers/PageWrapper";
+
+const ProgramsPage = () => {
+  return (
+    <PageWrapper>
+      <div className="container mx-auto px-4 py-16">
+        <h1 className="text-4xl font-bold mb-8">Programs and Services</h1>
+        <div className="bg-white rounded-lg shadow-md p-8">
+          <p className="text-xl">
+            More information about PACS programs to go here
+          </p>
+        </div>
+      </div>
+    </PageWrapper>
+  );
+};
+
+export default ProgramsPage;
