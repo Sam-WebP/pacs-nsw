c7d41ae8c5631cfedd4c639d73d5c04508ca16b2
Sam-WebP <113742608+Sam-WebP@users.noreply.github.com>
2025-08-24 16:44:21 +1000

    style: change content colour to improve accessibility

--- src/components/shared/headings/HeadingLg.tsx
+++ src/components/shared/headings/HeadingLg.tsx
@@ -9,11 +9,11 @@ const HeadingLg: React.FC<HeadingLgProps> = ({ children, color }) => {
   return (
     <div>
       <h1
-        className={`text-3xl ${
+        className={`text-2xl ${
           color === "white"
             ? "text-whiteColor"
             : "text-blackColor dark:text-blackColor-dark"
-        } md:text-5xl lg:text-size-50 2xl:text-5xl leading-10 md:leading-18 lg:leading-62px 2xl:leading-18 md:tracking-half lg:tracking-normal 2xl:tracking-half font-bold mb-15px`}
+        } md:text-4xl lg:text-4xl 2xl:text-4xl leading-10 md:leading-18 lg:leading-62px 2xl:leading-18 md:tracking-half lg:tracking-normal 2xl:tracking-half font-bold mb-15px`}
       >
         {children}
       </h1>
